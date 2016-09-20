d2f323703d226aedaee9a58d70bd82e04c27b55d
Pierre Dfc <piierre.d@live.fr>
2016-09-20 19:53:35 +0200

    TMApp + drawer items

--- app/src/main/java/fr/isen/twinmx/Activities/MainActivity.java
+++ app/src/main/java/fr/isen/twinmx/Activities/MainActivity.java
@@ -12,11 +12,14 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import fr.isen.twinmx.R;
+import fr.isen.twinmx.Util.TMDrawer;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    private TMDrawer drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +31,8 @@ public class MainActivity extends AppCompatActivity {
         this.setSupportActionBar(this.toolbar);
         this.setTitle(R.string.app_name);
 
-        //create the drawer and remember the `Drawer` result object
-        Drawer result = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .build();
+        this.drawer = new TMDrawer(savedInstanceState, this, this.toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
     }
 }
--- app/src/main/java/fr/isen/twinmx/Util/TMDrawer.java
+++ app/src/main/java/fr/isen/twinmx/Util/TMDrawer.java
@@ -16,9 +16,15 @@ import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
+import java.util.ArrayList;
+
+import fr.isen.twinmx.R;
 
 
 public class TMDrawer implements Drawer.OnDrawerItemClickListener {
+
     AccountHeader accountHeader;
     Drawer drawer;
 
@@ -26,17 +32,78 @@ public class TMDrawer implements Drawer.OnDrawerItemClickListener {
     Toolbar toolbar;
     Bundle savedInstanceState;
 
+    private ArrayList<IDrawerItem> items = new ArrayList<>();
+
     public TMDrawer(Bundle savedInstanceState, Activity activity, Toolbar toolbar) {
         this.savedInstanceState = savedInstanceState;
         this.activity = activity;
         this.toolbar = toolbar;
 
-       // setAccountHeader();
-       // setDrawer();
+        this.initDrawer();
+        this.drawer.setOnDrawerItemClickListener(this);
+    }
+
+    private void initDrawer()
+    {
+        this.initDrawerItems();
+        this.setAccountHeader();
+
+        //create the drawer and remember the `Drawer` result object
+        this.drawer = new DrawerBuilder()
+                .withActivity(this.activity)
+                .withRootView(R.id.drawer_container)
+                .withToolbar(this.toolbar)
+                .withTranslucentStatusBar(false)
+                .withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withAccountHeader(this.accountHeader)
+                .withCloseOnClick(true)
+                .addDrawerItems(items.toArray(new IDrawerItem[items.size()]))
+                .withSelectedItemByPosition(1)
+                .build();
+
+        this.drawer.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+    }
+
+    private void setAccountHeader()
+    {
+        // Create the AccountHeader
+        this.accountHeader = new AccountHeaderBuilder()
+                .withActivity(this.activity)
+                .withSelectionListEnabled(false)
+                .withSelectionListEnabledForSingleProfile(false)
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        this.accountHeader.setBackgroundRes(R.color.colorPrimary);
+    }
+
+    private void initDrawerItems()
+    {
+        addDrawerItem(R.string.drawer_home);
+        addDrawerItem(R.string.drawer_launch);
+        addDrawerItem(R.string.drawer_history);
+
+        this.items.add(new DividerDrawerItem());
+
+        addDrawerItem(R.string.drawer_instruction);
+        addDrawerItem(R.string.drawer_settings);
+    }
+
+    private void addDrawerItem(int resId)
+    {
+        this.items.add(new PrimaryDrawerItem().withName(resId));
     }
 
     @Override
     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-        return false;
+        drawer.closeDrawer();
+       //TODO
+        return true;
     }
 }
