d9181e53a7778c4da5a84f6fece8fbb3c4f69a5c
Pierre Dfc <piierre.d@live.fr>
2016-09-26 16:34:02 +0200

    Empty fragments

---
+++ app/src/main/java/fr/isen/twinmx/Fragments/HistoryFragment.java
@@ -0,0 +1,10 @@
+package fr.isen.twinmx.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by pierredfc.
+ */
+
+public class HistoryFragment extends Fragment {
+}
---
+++ app/src/main/java/fr/isen/twinmx/Fragments/SettingsFragment.java
@@ -0,0 +1,10 @@
+package fr.isen.twinmx.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by pierredfc.
+ */
+public class SettingsFragment extends Fragment {
+
+}
