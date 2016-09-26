bb79303fe715fb5707c93fb41f576315cae8e6d6
Pierre Dfc <piierre.d@live.fr>
2016-09-26 14:59:23 +0200

    Custom BottomNavigation

--- app/src/main/java/fr/isen/twinmx/Activities/MainActivity.java
+++ app/src/main/java/fr/isen/twinmx/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -20,6 +21,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import fr.isen.twinmx.Fragments.BluetoothFragment;
 import fr.isen.twinmx.R;
+import fr.isen.twinmx.Util.TMBottomNavigation;
 import fr.isen.twinmx.Util.TMDrawer;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,19 +42,10 @@ public class MainActivity extends AppCompatActivity {
         this.setSupportActionBar(this.toolbar);
         this.setTitle(R.string.app_name);
 
+        final TMBottomNavigation nav = new TMBottomNavigation(this.navigation, savedInstanceState, this, this.toolbar);
+        //nav.initNavigation();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-
-        // Create items
-        AHBottomNavigationItem acquisition = new AHBottomNavigationItem(R.string.drawer_launch, R.drawable.ic_launch_black_24dp, R.color.colorAccent);
-        AHBottomNavigationItem history = new AHBottomNavigationItem(R.string.drawer_history, R.drawable.ic_history_black_24dp, R.color.colorAccent);
-        AHBottomNavigationItem instruction = new AHBottomNavigationItem(R.string.drawer_instruction, R.drawable.ic_help_black_24dp, R.color.colorAccent);
-        AHBottomNavigationItem settings = new AHBottomNavigationItem(R.string.drawer_settings, R.drawable.ic_settings_applications_black_24dp, R.color.colorAccent);
-
-        // Add items
-        navigation.addItem(acquisition);
-        navigation.addItem(history);
-        navigation.addItem(instruction);
-        navigation.addItem(settings);
 
         this.launchHome();
     }
