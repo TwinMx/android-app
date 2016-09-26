a26ef5398bbae1457e07d324744eaef39d759b80
Pierre Dfc <piierre.d@live.fr>
2016-09-26 17:15:49 +0200

    RecyclerView adapter and holder

---
+++ app/src/main/java/fr/isen/twinmx/model/History.java
@@ -0,0 +1,15 @@
+package fr.isen.twinmx.model;
+
+import java.util.Date;
+
+/**
+ * Created by pierredfc.
+ */
+
+public class History {
+
+    private String name;
+    private Date date;
+    private String image;
+
+}
