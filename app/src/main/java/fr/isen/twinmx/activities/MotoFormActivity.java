3ffc931437169b1a109bfae4ab2f8ac6d634e48f
Pierre Dfc <piierre.d@live.fr>
2017-01-19 10:59:32 +0100

    Take picture

--- app/src/main/java/fr/isen/twinmx/activities/MotoFormActivity.java
+++ app/src/main/java/fr/isen/twinmx/activities/MotoFormActivity.java
@@ -1,8 +1,16 @@
 package fr.isen.twinmx.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ImageView;
 
 import butterknife.BindView;
@@ -15,6 +23,8 @@ import fr.isen.twinmx.R;
  */
 
 public class MotoFormActivity extends AppCompatActivity {
+
+    private static int REQUEST_IMAGE_CAPTURE = 1;
 
     @OnClick(R.id.form_back)
     public void back()
@@ -31,7 +41,10 @@ public class MotoFormActivity extends AppCompatActivity {
     @OnClick(R.id.form_moto_launchCamera)
     public void launchCamera()
     {
-        // TODO
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     @OnClick(R.id.form_moto_create)
@@ -53,10 +66,69 @@ public class MotoFormActivity extends AppCompatActivity {
     ImageView photo;
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        if(requestCode == REQUEST_IMAGE_CAPTURE)
+        {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_moto_form);
         ButterKnife.bind(this);
+        this.locationpermission();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Log.d("CameraDemo", "Pic take");
+        }
+    }
+
+    private void locationpermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this
+                ,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_IMAGE_CAPTURE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 }
