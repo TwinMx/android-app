e267aaa79bcaa4f3fa35a1f860f632d8b5c708d4
Pierre Dfc <piierre.d@live.fr>
2017-01-19 22:53:00 +0100

    Moto detail beginning

--- app/src/main/java/fr/isen/twinmx/activities/MotoDetailActivity.java
+++ app/src/main/java/fr/isen/twinmx/activities/MotoDetailActivity.java
@@ -5,6 +5,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -13,8 +18,10 @@ import java.util.Date;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import fr.isen.twinmx.R;
+import fr.isen.twinmx.TMApplication;
 import fr.isen.twinmx.database.MotoRepository;
 import fr.isen.twinmx.database.model.Moto;
+import fr.isen.twinmx.util.CircleTransformation;
 
 /**
  * Created by pierredfc.
@@ -27,6 +34,15 @@ public class MotoDetailActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.moto_detail_photo)
+    ImageView photo;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,5 +55,11 @@ public class MotoDetailActivity extends AppCompatActivity {
 
         this.setSupportActionBar(this.toolbar);
         this.setTitle(this.moto.getName());
+
+        Picasso.with(TMApplication.getContext())
+                .load(moto.getImage())
+                .transform(new CircleTransformation())
+                .placeholder(R.drawable.ic_motorcycle_black_24dp)
+                .into(this.photo);
     }
 }
